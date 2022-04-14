35fdf6c9cfc609baecee76ad2d6877ccc4502f60
Sarthak Vinchurkar <sarthakv882@gmail.com>
2022-04-15 02:03:47 +0530

    Add files via upload

--- testapi.py
+++ testapi.py
@@ -1,41 +1,25 @@
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-
 @app.route('/')
 def hello():
-    return "hello"
 
-@app.route('/bfhl', methods = ['POST'])
-def bfhl():
     if request.method == 'POST':
-        input = request.get_json()
-        numbers = input['numbers']
-        # numbers=[1,2,3,4,5,6,"3",4]
-        # test(numbers)
-        odd = []
-        even = []
-        is_success = True
-        for i in numbers:
-            try:
-                if int(i) % 2 == 0:
-                    even.append(int(i))
-                else:
-                    odd.append(int(i))
-            except:
-                is_success = False
-                # print("failed")
-                return jsonify(is_success = str(is_success).lower(), user_id = "sarthak_vinchurkar_08082002")
-                # return jsonify({"is_success": str(is_success).lower(), "user_id": "sarthak_vinchurkar_08082002"})
+        return "post method on /"
+    else:
+        return "hello"
 
-        # print("printing jsonified doc")
-        return jsonify(is_success = str(is_success).lower(), user_id = "sarthak_vinchurkar_08082002", odd = odd, even = even)
-
-
-def test(numbers):
+@app.route('/bfhl', methods=['POST'])
+def bfhl():
+    # if request.method == 'POST':
+    input = request.get_json()
+    # input = request.form
+    print(input)
+    numbers = input['numbers']
+    # numbers=[1,2,3,4,5,6,"3",4]
+    # test(numbers)
     odd = []
     even = []
-    dicct = {}
     is_success = True
     for i in numbers:
         try:
@@ -45,13 +29,45 @@ def test(numbers):
                 odd.append(int(i))
         except:
             is_success = False
-            break
-    dicct['is_success'] = str(is_success).lower()
-    dicct['user_id'] = 'sarthak_vinchurkar_08082002'
+            # print("failed")
+            return jsonify(is_success = str(is_success).lower(), user_id = "sarthak_vinchurkar_08082002")
+            # return jsonify({"is_success": str(is_success).lower(), "user_id": "sarthak_vinchurkar_08082002"})
 
-    if(is_success):
-        dicct["odd"] = odd
-        dicct['even'] = even
-    return dicct
+    # print("printing jsonified doc")
+    return jsonify(is_success = str(is_success).lower(), user_id = "sarthak_vinchurkar_08082002", odd = odd, even = even)
+
+@app.route('/returnjson', methods = ['GET'])
+def ReturnJSON():
+    if(request.method == 'GET'):
+        data = {
+            "Modules" : 15,
+            "Subject" : "Data Structures and Algorithms",
+        }
+  
+        return jsonify(data)
+
+# app.run()
+
+# def test(numbers):
+#     odd = []
+#     even = []
+#     dicct = {}
+#     is_success = True
+#     for i in numbers:
+#         try:
+#             if int(i) % 2 == 0:
+#                 even.append(int(i))
+#             else:
+#                 odd.append(int(i))
+#         except:
+#             is_success = False
+#             break
+#     dicct['is_success'] = str(is_success).lower()
+#     dicct['user_id'] = 'sarthak_vinchurkar_08082002'
+
+#     if(is_success):
+#         dicct["odd"] = odd
+#         dicct['even'] = even
+#     return dicct
 
 # pprint(test([1,2,3,4,5,6,"3",4]))
